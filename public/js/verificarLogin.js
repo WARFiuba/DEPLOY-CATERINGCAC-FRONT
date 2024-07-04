10b4be8bcfe470a02725eed9c04270b10bbe46fb
William Ramirez <86168463+WARFiuba@users.noreply.github.com>
2024-07-04 09:49:25 -0300

    arreglo

--- public/js/verificarLogin.js
+++ public/js/verificarLogin.js
@@ -1,16 +1,29 @@
-const token = localStorage.getItem('token');
+async function verificarLogin() {
+    const token = localStorage.getItem('token');
 
-if (!token){
-    window.location.href = '/login'
-} else {
-    const response = fetch(`http://backendcac.alwaysdata.net/${token}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
+    if (!token) {
+        window.location.href = '/login';
+    } else {
+        try {
+            const response = await fetch(`http://backendcac.alwaysdata.net/verificarToken/${token}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (!response.ok) {
+                localStorage.clear();
+                window.location.href = '/login';
+            } else {
+                const data = await response.json();
+                console.log("Fetch response data:", data);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            localStorage.clear();
+            window.location.href = '/login';
         }
-    });
-    if (!response.ok) {
-        localStorage.clear();
-        window.location.href = '/login'
     }
 }
+
+verificarLogin();
